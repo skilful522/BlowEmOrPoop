bf35a41410e547555cb61aef981af28b41328aa5
Stanislav Sergienya <skilful522@gmail.com>
2022-03-28 15:03:13 +0300

    add support for sound/images

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,21 @@ const path = require('path')
 
 module.exports = {
     entry: './index.js',
+    module: {
+        rules: [{
+            test: /\.(png)$/,
+            use: [{
+                loader: 'file-loader',
+                options: {}
+            }, {
+                test: /\.(mp3|ogg|wav)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]'
+                }
+            }]
+        }]
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.js'
